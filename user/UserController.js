4b5dc8a91cf0f0120454a6cfc4674a90be2dfe48
Pratik Sethi <pratik.root@gmail.com>
2017-04-09 21:23:23 +0530

    Added the routes for create and find

--- user/UserController.js
+++ user/UserController.js
@@ -1,10 +1,32 @@
-//UserController.js
+// UserController.js
 
 var express = require('express');
 var router = express.Router();
-var body-parser = require('body-parser');
+var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({extended : true}));
 var User = require('./User');
+
+// CREATE A NEW USER
+router.post('/', function(req, res){
+    User.create({
+    	name : req.body.name,
+	email : req.body.email,
+	password : req.body.password    
+    },
+    function(err, user){
+	if(err) return res.status(500).send("There was a problem adding the information to the database. ");
+	res.status(200).send(user);
+    });
+});
+
+// RETURNS ALL THE USERS IN THE DATABASE
+router.get('/', function(req, res){
+    User.find({}, function (err, users){
+    	if(err) return res.status(500).send("There was a problem finding the users ");
+	res.status(200).send(users);
+    });
+});
+
 module.exports = router;
 
 
