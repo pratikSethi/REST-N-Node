7718ec6af41c5250d121a75d6d0e5c90c03740c2
Pratik Sethi <pratik.root@gmail.com>
2017-04-10 03:11:28 +0530

    Added 3 more routes to the app

--- user/UserController.js
+++ user/UserController.js
@@ -27,6 +27,29 @@ router.get('/', function(req, res){
     });
 });
 
+// FETCHES A SINGLE USER FROM THE DATABASE
+router.get('/:id', function(req, res){
+    User.findById({req.params.id}, function(err, user){
+	if(err) return res.status(500).send("There was a problem finding the user");
+	if(!user) return res.status(404).send("No user found. ");
+	res.status(200).send(user); 	
+    });
+});
+
+// UPDATES A SINGLE USER IN THE DATABASE
+router.get('/:id', function(req, res){
+    User.findByIdAndUpdate({req.params.id}, req.body, {new : true}, function(err, user){
+        if(err) return res.status(500).send("There was a problem updating the user");
+	res.status(200).send(user);	
+    });
+});
+
+// DELETES A SINGLE USER FROM THE DATABASE
+router.delete('/:id', function(req, res){
+    User.findByIdAndRemove({req.params.id}, function(err, user){
+        if(err) return res.status(500).send("There was a problem deleting the user");
+	res.status(200).send("User " + user.name + " was deleted");
+    });
+});
+
 module.exports = router;
-
-
