72104c2151238cbdcfdf9dc5b705b227eb96052c
Pratik Sethi <pratik.root@gmail.com>
2017-04-09 20:27:20 +0530

    Added the UserController

---
+++ user/UserController.js
@@ -0,0 +1,10 @@
+//UserController.js
+
+var express = require('express');
+var router = express.Router();
+var body-parser = require('body-parser');
+router.use(bodyParser.urlencoded({extended : true}));
+var User = require('./User');
+module.exports = router;
+
+
